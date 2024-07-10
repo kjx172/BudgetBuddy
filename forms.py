084230c51ef119e60c313ccd32f1a1156d530ac4
kuruefebeste <bestekuruefe@gmail.com>
2024-07-10 15:44:56 -0400

    added text to the home page, edited the form, created a static folder for the css file

--- forms.py
+++ forms.py
@@ -18,8 +18,8 @@ class BudgetForm(FlaskForm):
     savings = DecimalField('How much money do you set aside for savings each month?', validators=[DataRequired(), NumberRange(min=0)])
     
     # Variable Expenses
-    food = DecimalField('How much do you spend on groceries and dining out per month?', validators=[DataRequired(), NumberRange(min=0)])
-    entertainment = DecimalField('What is your monthly expense for entertainment, including concerts, music downloads, and movies?', validators=[DataRequired(), NumberRange(min=0)])
+    food = DecimalField('How much do you spend on groceries or a meal plan per month?', validators=[DataRequired(), NumberRange(min=0)])
+    entertainment = DecimalField('What is your monthly expense for entertainment, including dining out, concerts, music downloads, and movies?', validators=[DataRequired(), NumberRange(min=0)])
     health_personal_care = DecimalField('What is your monthly cost for medical expenses, prescriptions, hair, nails, and health club memberships?', validators=[DataRequired(), NumberRange(min=0)])
     clothing_laundry = DecimalField('How much do you spend on clothing and laundry/dry cleaning per month?', validators=[DataRequired(), NumberRange(min=0)])
     debt_payments = DecimalField('How much is your monthly payment for credit card debt?', validators=[DataRequired(), NumberRange(min=0)])
