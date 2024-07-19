542a386d3ffef0cfa7ee4b28e6d5fa48ac505e40
kuruefebeste <bestekuruefe@gmail.com>
2024-07-19 18:00:29 -0400

    fixed Bud

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from forms import BudgetForm
 from openai import OpenAI
+import openai
 import git
 import string
 import secrets
@@ -18,11 +19,6 @@ load_dotenv()
 
 # Fetch the API key from the environment variable
 api_key = os.getenv('OPENAI_API_KEY')
-
-# Function to generate a random secret key
-def generate_random_key(length=24):
-    alphabet = string.ascii_letters + string.digits + '!@#$%^&*(-_=+)'
-    return ''.join(secrets.choice(alphabet) for _ in range(length))
 
 # Ensure the api_key is provided
 if not api_key:
@@ -34,14 +30,11 @@ client = OpenAI(api_key=api_key)
 app = Flask(__name__)
 
 #configures the secret key to be random and connecting database.db to SQLalchamey
-app.config['SECRET_KEY'] = generate_random_key()
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
 #creates a database for the app, checks if passwords match
 db = SQLAlchemy(app)
-with app.app_context():
-    db.create_all()
-    print("Database tables created.")
 bcrypt = Bcrypt(app)
 
 #makes pages login restricted
@@ -67,80 +60,33 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
 
+class Budget(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    financial_discipline = db.Column(db.String(100), nullable=False)
+    spending_habits = db.Column(db.String(100), nullable=False)
+    saving_importance = db.Column(db.String(100), nullable=False)
+    short_term_savings = db.Column(db.Float, nullable=False)
+    long_term_savings = db.Column(db.Float, nullable=False)
+    investments = db.Column(db.Float, nullable=False)
+    income = db.Column(db.Float, nullable=False)
+    housing_utilities = db.Column(db.Float, nullable=False)
+    communication = db.Column(db.Float, nullable=False)
+    transportation = db.Column(db.Float, nullable=False)
+    education = db.Column(db.Float, nullable=False)
+    savings = db.Column(db.Float, nullable=False)
+    food = db.Column(db.Float, nullable=False)
+    entertainment = db.Column(db.Float, nullable=False)
+    health_personal_care = db.Column(db.Float, nullable=False)
+    clothing_laundry = db.Column(db.Float, nullable=False)
+    debt_payments = db.Column(db.Float, nullable=False)
+
 def validate_username(username):
     existing_user_username = User.query.filter_by(username=username).first()
     if not existing_user_username:
         return True
-    
-# Function to create the database table
-def create_table():
-    conn = sqlite3.connect('budgetbuddy.db')
-    c = conn.cursor()
-    c.execute('''
-        CREATE TABLE IF NOT EXISTS budget (
-            id INTEGER PRIMARY KEY,
-            age INTEGER,
-            financial_discipline TEXT,
-            spending_habits TEXT,
-            saving_importance TEXT,
-            short_term_savings REAL,
-            long_term_savings REAL,
-            investments REAL,
-            income REAL,
-            housing_utilities REAL,
-            communication REAL,
-            transportation REAL,
-            education REAL,
-            savings REAL,
-            food REAL,
-            entertainment REAL,
-            health_personal_care REAL,
-            clothing_laundry REAL,
-            debt_payments REAL
-        )
-    ''')
-    conn.commit()
-    conn.close()
 
-# Function to insert data into the database
-def insert_budget_data(form_data):
-    #try creating table if it doesnt exist
-    create_table()
-    conn = sqlite3.connect('budgetbuddy.db')
-    c = conn.cursor()
-    c.execute('''
-        INSERT INTO budget (age, financial_discipline, spending_habits, saving_importance, short_term_savings, long_term_savings, investments, income, housing_utilities, communication, transportation, education, savings, food, entertainment, health_personal_care, clothing_laundry, debt_payments)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ''', [
-        int(form_data['age']),
-        form_data['financial_discipline'],
-        form_data['spending_habits'],
-        form_data['saving_importance'],
-        float(form_data['short_term_savings']),
-        float(form_data['long_term_savings']),
-        float(form_data['investments']),
-        float(form_data['income']),
-        float(form_data['housing_utilities']),
-        float(form_data['communication']),
-        float(form_data['transportation']),
-        float(form_data['education']),
-        float(form_data['savings']),
-        float(form_data['food']),
-        float(form_data['entertainment']),
-        float(form_data['health_personal_care']),
-        float(form_data['clothing_laundry']),
-        float(form_data['debt_payments'])
-    ])
-    conn.commit()
-    conn.close()
-
-def get_last_row():
-    conn = sqlite3.connect("budgetbuddy.db")
-    c = conn.cursor()
-    c.execute('SELECT * FROM budget ORDER BY id DESC LIMIT 1')
-    last_row = c.fetchone()
-    conn.close()
-    return last_row
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/login', methods=['GET', 'POST'])
@@ -151,13 +97,12 @@ def login():
         form_type = request.form.get('form_type')
 
         if form_type == 'sign-up':  # Check if sign-up form is submitted
-            # hashes password and uses that + username to store user in db
-
             #if username already exists
             if not validate_username(username):
                 flash('Username is already in use.')
                 return redirect(url_for('login'))
             
+            # hashes password and uses that + username to store user in db
             hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
             new_user = User(username=username, password=hashed_password)
             db.session.add(new_user)
@@ -172,44 +117,46 @@ def login():
 
             #if the username is valid and the password entered matches the username's hashed password
             if user and bcrypt.check_password_hash(user.password, password):
+                login_user(user)
                 session['user_id'] = user.id
+                print(f"Logged in user ID: {current_user.id}")
                 return redirect(url_for('form'))
             else:
                 flash('Invalid credentials. Please try again.')
 
     return render_template('login.html')
 
-@app.route('/about', methods=['GET', 'POST'])
-def about():
-    return render_template('about.html')
 
 @app.route('/form', methods=['GET', 'POST'])
 @login_required
-def form():        
+def form():
     form = BudgetForm()
     if form.validate_on_submit():
-        form_data = {
-            'age': form.age.data,
-            'financial_discipline': form.financial_discipline.data,
-            'spending_habits': form.spending_habits.data,
-            'saving_importance': form.saving_importance.data,
-            'short_term_savings': form.short_term_savings.data,
-            'long_term_savings': form.long_term_savings.data,
-            'investments': form.investments.data,
-            'income': form.income.data,
-            'housing_utilities': form.housing_utilities.data,
-            'communication': form.communication.data,
-            'transportation': form.transportation.data,
-            'education': form.education.data,
-            'savings': form.savings.data,
-            'food': form.food.data,
-            'entertainment': form.entertainment.data,
-            'health_personal_care': form.health_personal_care.data,
-            'clothing_laundry': form.clothing_laundry.data,
-            'debt_payments': form.debt_payments.data,
-        }
+        budget = Budget(
+            user_id=current_user.id,
+            age=form.age.data,
+            financial_discipline=form.financial_discipline.data,
+            spending_habits=form.spending_habits.data,
+            saving_importance=form.saving_importance.data,
+            short_term_savings=form.short_term_savings.data,
+            long_term_savings=form.long_term_savings.data,
+            investments=form.investments.data,
+            income=form.income.data,
+            housing_utilities=form.housing_utilities.data,
+            communication=form.communication.data,
+            transportation=form.transportation.data,
+            education=form.education.data,
+            savings=form.savings.data,
+            food=form.food.data,
+            entertainment=form.entertainment.data,
+            health_personal_care=form.health_personal_care.data,
+            clothing_laundry=form.clothing_laundry.data,
+            debt_payments=form.debt_payments.data,
+        )
 
-        insert_budget_data(form_data)
+        db.session.add(budget)
+        db.session.commit()
+
         return redirect(url_for('summary'))
     return render_template('form.html', form=form)
 
@@ -233,19 +180,39 @@ def chatbot():
     if user_input.lower() == 'exit':
         response = {"message": "Goodbye!"}
     else:
-        last_row = get_last_row()
-        if last_row:
-            personality = last_row[1:5]
-            financial_history = [float(exp) for exp in last_row[5:8]] 
-            income = float(last_row[8])
-            expenses = [float(exp) for exp in last_row[9:19]]
-            initial_message = f"User's age: ${personality[0]:.2f}\n" \
+        last_budget = Budget.query.filter_by(user_id=current_user.id).order_by(Budget.id.desc()).first()
+        if last_budget:
+            personality = [
+                last_budget.age,
+                last_budget.financial_discipline,
+                last_budget.spending_habits,
+                last_budget.saving_importance
+            ]
+            financial_history = [
+                last_budget.short_term_savings,
+                last_budget.long_term_savings,
+                last_budget.investments
+            ]
+            income = last_budget.income
+            expenses = [
+                last_budget.housing_utilities,
+                last_budget.communication,
+                last_budget.transportation,
+                last_budget.education,
+                last_budget.savings,
+                last_budget.food,
+                last_budget.entertainment,
+                last_budget.health_personal_care,
+                last_budget.clothing_laundry,
+                last_budget.debt_payments
+            ]
+            initial_message = f"User's age: {personality[0]}\n" \
                               f"How often does the user follow a budget?: {personality[1]}\n" \
                               f"How would the user describe their spending habits?: {personality[2]}\n" \
-                              f"How important is it for the user to save a portion of your income regularly: {personality[3]}\n" \
-                              f"The user's short-term savings (the money they have saved for expenses they'll face within the next year (e.g., groceries, textbooks, or travel)): ${financial_history[0]:.2f}\n" \
-                              f"The user's long-term savings (the money they have saved for goals more than a year away (e.g., savings for post-graduation plans, a car, or future investments)): ${financial_history[1]:.2f}\n" \
-                              f"The money the user currently has invested (e.g., in stocks, bonds, or mutual funds): ${financial_history[2]:.2f}\n" \
+                              f"How important is it for the user to save a portion of their income regularly: {personality[3]}\n" \
+                              f"The user's short-term savings: ${financial_history[0]:.2f}\n" \
+                              f"The user's long-term savings: ${financial_history[1]:.2f}\n" \
+                              f"The money the user currently has invested: ${financial_history[2]:.2f}\n" \
                               f"User's monthly income: ${income:.2f}\n" \
                               f"Housing and Utilities expenses: ${expenses[0]:.2f}\n" \
                               f"Communication expenses: ${expenses[1]:.2f}\n" \
@@ -259,8 +226,8 @@ def chatbot():
                               f"Debt Payments expenses: ${expenses[9]:.2f}\n"
             
         else:
-            initial_message = "The user hasn't filled out the form on the home page yet. Suggest the user fill out the form."
-        
+            initial_message = "The user hasn't filled out the form yet."
+
         try:
             response = client.chat.completions.create(
                 model="gpt-4",
@@ -272,6 +239,7 @@ def chatbot():
                     {"role": "system", "content": "Offer practical tips and examples whenever possible."},
                     {"role": "system", "content": initial_message},
                     {"role": "system", "content": "Your audience is college students. Tailor your responses to the finances and situations of the typical American college student."},
+                    {"role": "system", "content": "If you don't have the user's budget information, direct them to fill out the form in the form page first."},
                     {"role": "system", "content": "Remember that housing and utilities, communication, transportation, education, food, and health and personal care expenses are the user's needs. Entertainment and clothing expenses are the user's wants. Debt payment and saving expenses are the user's savings/debt repayments."},
                     {"role": "system", "content": "If the user asks about budgeting, tell them about the 50/30/20 budget rule."},
                     {"role": "system", "content": "Ideally, the user should allocate 50%% of their income to their needs, 30%% to their wants, and 20%% to their savings and debt repayments."},
@@ -280,24 +248,43 @@ def chatbot():
                     {"role": "user", "content": user_input}
                 ]
             )
+       
             message = response.choices[0].message.content.strip()
-            return jsonify({"message": message})
+            response = {"message": message}
         except Exception as e:
             print(f"Error: {str(e)}")
-            return jsonify({"message": "Something went wrong"})
+            response = {"message": "Something went wrong"}
+
+    return jsonify(response)
 
 @app.route('/summary')
 @login_required
 def summary():
-    last_row = get_last_row()
-    if last_row:
-        income = float(last_row[8])
-        expenses = last_row[9:19]
-        financial_history = last_row[5:8]
+    last_budget = Budget.query.filter_by(user_id=current_user.id).order_by(Budget.id.desc()).first()
 
-        needs = sum([float(expenses[0]), float(expenses[5]), float(expenses[2]), float(expenses[1]), float(expenses[3]), float(expenses[7])])
-        wants = sum([float(expenses[6]), float(expenses[8])])
-        savings_or_debt = sum([float(expenses[4]), float(expenses[9])])
+    if last_budget:
+        income = last_budget.income
+        expenses = [
+            last_budget.housing_utilities,
+            last_budget.communication,
+            last_budget.transportation,
+            last_budget.education,
+            last_budget.savings,
+            last_budget.food,
+            last_budget.entertainment,
+            last_budget.health_personal_care,
+            last_budget.clothing_laundry,
+            last_budget.debt_payments
+        ]
+        financial_history = [
+            last_budget.short_term_savings,
+            last_budget.long_term_savings,
+            last_budget.investments
+        ]
+
+        needs = sum([expenses[0], expenses[5], expenses[2], expenses[1], expenses[3], expenses[7]])
+        wants = sum([expenses[6], expenses[8]])
+        savings_or_debt = sum([expenses[4], expenses[9]])
 
         actual_amounts = {
             'Needs': needs,
@@ -324,13 +311,19 @@ def summary():
         }
     else:
         income = 0
-        expenses = []
+        expenses = [0] * 10  # Default empty expenses list
+        financial_history = [0] * 3  # Default empty financial history
         actual_amounts = {'Needs': 0, 'Wants': 0, 'Savings or Debt Repayment': 0}
         actual_percentages = {'Needs': 0, 'Wants': 0, 'Savings or Debt Repayment': 0}
         ideal_amounts = {'Needs': 0, 'Wants': 0, 'Savings or Debt Repayment': 0}
         ideal_percentages = {'Needs': 0, 'Wants': 0, 'Savings or Debt Repayment': 0}
 
     return render_template('summary.html', income=income, expenses=expenses, financial_history=financial_history, actual_amounts=actual_amounts, actual_percentages=actual_percentages, ideal_amounts=ideal_amounts, ideal_percentages=ideal_percentages)
+
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')
+
 
 @app.route("/update_server", methods=['POST'])
 def webhook():
@@ -344,5 +337,4 @@ def webhook():
 
 
 if __name__ == '__main__':
-    create_table()  # Create the database table if it doesn't exist
     app.run(debug=True)
